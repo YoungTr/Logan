dd76d05fe2b32e3cfc59c0433121ad10e76345fd
YoungTr <tangyan1004@gmail.com>
2022-02-20 16:59:25 +0800

    升级gradle

--- Example/Logan-Android/xBuildInstall.sh
+++ Example/Logan-Android/xBuildInstall.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "------------------start build------------------"
-./gradlew :app:assembleDebug
-echo "------------------build finish------------------"
-echo "------------------start install------------------"
-adb install -r ./app/build/outputs/apk/debug/app-debug.apk
+./gradlew :app:iD
 adb shell am start -n test.logan.dianping.com.logan/test.logan.dianping.com.logan.MainActivity
